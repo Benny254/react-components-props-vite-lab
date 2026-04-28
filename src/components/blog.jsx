7af6bfb08b8d86b3a2ffce0bc48dccebc80d5413
Benny <asshimbenny01@gmail.com>
2026-04-28 12:30:11 +0300

    add styling for blog plus the image

---
+++ src/components/blog.jsx
@@ -0,0 +1,21 @@
+const blog = {
+  name: "My Blog",
+  image: "public/images/image.png",
+  about: "Welcome to my blog where I share my thoughts.",
+  posts: [
+    {
+      id: 1,
+      title: "First Post",
+      date: "April 20, 2026",
+      preview: "This is my first article preview."
+    },
+    {
+      id: 2,
+      title: "React is Fun",
+      date: "April 25, 2026",
+      preview: "React makes building UIs simple."
+    }
+  ]
+};
+
+export default blog;
