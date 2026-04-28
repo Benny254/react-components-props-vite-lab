52544f67e582b2fd32c7b7440e09b19450a94366
Benny <asshimbenny01@gmail.com>
2026-04-28 12:47:13 +0300

    updated (add styling for blog plus the image)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 function About(props) {
+  const imageUrl = props.image || "https://via.placeholder.com/215";
+
   return (
     <aside>
-      <img src={props.image} alt="blog logo" />
+      <img src={imageUrl} alt="blog logo" />
       <p>{props.about}</p>
     </aside>
   );
